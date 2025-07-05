de86b7d8e12ab863fe525f530e692d5bbabe1d34
GitHub Action <you@example.com>
2025-07-05 11:26:59 +0330

    add colorfull output feature and add toml file for pypi package

---
+++ README.md
@@ -0,0 +1,39 @@
+# Catsay CLI
+# ![License](https://img.shields.io/badge/license-MIT-green) [![PyPI](https://img.shields.io/pypi/v/catsay-cli)](https://pypi.org/project/catsay-cli/) ![Package Size](https://img.shields.io/badge/size-5.0KB-red)
+
+A fun CLI tool inspired by cowsay, but with cute ASCII cats! 🐱
+
+---
+# 🚀 Features
+
+- 7 different ASCII cat designs
+- Colorful Output: 10 different colors to choose from
+
+---
+## 📦 Installation
+
+From PyPI
+pip install catsay-cli---
+
+## ⚙️ Usage
+Simple
+catsay 'Hello World!'![Screenshot](./assets/photos//screenshot.png)
+
+Color Options
+catsay 'Hello World!' -c red![Screenshot](./assets/photos//screenshot.png)
+
+catsay --list-colors![Screenshot](./assets/photos//screenshot.png)
+
+---
+## 🆘 Help
+catsay --help---
+## 🛠 Contributing
+Feel free to contribute by:
+
+- Adding new cat ASCII art designs
+- Suggesting new color options
+- Improving the code
+- Reporting bugs
+---
+## 📄 License
+This project is licensed under the MIT License – see the [LICENSE](./LICENSE) file for details.
---
+++ catsay/cli.py
@@ -0,0 +1,72 @@
+import argparse
+import random
+from rich import print
+import builtins
+from .cat_art import cats
+from rich.console import Console
+from rich.text import Text
+
+WIDTH = 40
+AVAILABLE_COLORS = [
+    "red",
+    "green",
+    "yellow",
+    "blue",
+    "magenta",
+    "cyan",
+    "white",
+    "black",
+    "bright_white",
+    "bright_black"
+]
+
+def make_balloon(text: str) -> str:
+    width = len(text)
+    if len(text) > WIDTH:
+        print("[bold red] Write shorter! [/bold red]")
+        return 0
+
+    top = "  " + "_" * (width + 2)
+    bottom = "  " + "-" * (width + 2)
+
+    balloon = [top]
+    balloon.append(f"  |{text.ljust(width)}|")
+    balloon.append(bottom)
+    return "\n".join(balloon)
+
+def list_colors():
+    Console.print("[bold underline]Colors available:[/bold underline]")
+    for color in AVAILABLE_COLORS:
+        Console.print(f"[{color}]{color}[/]")
+
+def main():
+    parser = argparse.ArgumentParser(prog="catsay", description="Catsay CLI: Display your message with a talking ASCII cat!")
+    parser.add_argument("message", nargs="+", help="The message your cat will say")
+    parser.add_argument("-c", "--color", help="Color of the output text (e.g. red, blue, magenta)")
+    parser.add_argument("--list-colors", action="store_true", help="Show all supported colors")
+    args = parser.parse_args()
+
+    if args.list_colors:
+        list_colors()
+        return
+    
+    if not args.message:
+        parser.print_help()
+        return
+    
+    text = " ".join(args.message)
+    ballon= make_balloon(text)
+    if ballon == 0 : return
+    cat = random.choice(cats)
+    result = ballon + "\n" + cat
+
+    if args.color:
+        if args.color not in AVAILABLE_COLORS:
+            Console.print(f"[red]❌ Color '{args.color}' is not valid. To see available colors, use:[/] [bold]catsay --list-colors[/bold]")
+            return
+        Console.print(Text(result, style=args.color))
+    else:
+        builtins.print(result)
+
+if __name__ == "__main__":
+    main()
